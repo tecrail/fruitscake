23de9ecb1fa3220e9ba492af8677a5b47f120f80
Andrea Dal Ponte <dalpo85@gmail.com>
2010-12-27 01:05:26 +0100

    Refactoring users from plugin to base app

--- app/app_model.php
+++ app/app_model.php
@@ -1,46 +1,22 @@
 <?php
-/* SVN FILE: $Id: app_model.php 6311 2008-01-02 06:33:52Z phpnut $ */
 
-/**
- * Application model for Cake.
- *
- * This file is application-wide model file. You can put all
- * application-wide model-related methods here.
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) :  Rapid Development Framework <http://www.cakephp.org/>
- * Copyright 2005-2008, Cake Software Foundation, Inc.
- *								1785 E. Sahara Avenue, Suite 490-204
- *								Las Vegas, Nevada 89104
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @filesource
- * @copyright		Copyright 2005-2008, Cake Software Foundation, Inc.
- * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package			cake
- * @subpackage		cake.app
- * @since			CakePHP(tm) v 0.2.9
- * @version			$Revision: 6311 $
- * @modifiedby		$LastChangedBy: phpnut $
- * @lastmodified	$Date: 2008-01-01 22:33:52 -0800 (Tue, 01 Jan 2008) $
- * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
- */
-
-/**
- * Application model for Cake.
- *
- * Add your application-wide methods in the class below, your models
- * will inherit them.
- *
- * @package		cake
- * @subpackage	cake.app
- */
 class AppModel extends Model {
 
-//  public $actsAs = array('Containable');
+    public $actsAs = array('Containable');
+
+    public function paginateCount($conditions = array(), $recursive = 0, $extra = array()) {
+        $parameters = compact('conditions');
+        if ($recursive != $this->recursive) {
+            $parameters['recursive'] = $recursive;
+        }
+        if (isset($extra['type']) && isset($this->_findMethods[$extra['type']])) {
+            $extra['operation'] = 'count';
+            return $this->find($extra['type'], array_merge($parameters, $extra));
+        } else {
+            return $this->find('count', array_merge($parameters, $extra));
+        }
+    }
+
 //  public $translateColumns = false;
 //  public $recursive = -1;
 //  protected $_locale = false;
@@ -64,6 +40,4 @@ class AppModel extends Model {
 //    }
 //    return $queryData;
 //  }
-
 }
-?>
