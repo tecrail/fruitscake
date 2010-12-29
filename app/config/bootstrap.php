2d6059b0d75b032741ce66ac13b073ee7dbe711d
Andrea Dal Ponte <dalpo85@gmail.com>
2010-12-29 15:35:45 +0100

    App renaming

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
 Configure::write('App.defaultEmail', 'noreply@' . env('HTTP_HOST'));
-Configure::write('App.baseTitle', 'Confectioner site name');
+Configure::write('App.baseTitle', 'Fruitscake site name');
