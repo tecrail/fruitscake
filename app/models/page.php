1cc87971fc40cf9006b3750dcd79d50b1b1e70b1
Andrea Dal Ponte <dalpo85@gmail.com>
2011-01-06 16:47:46 +0100

    acts as sluggable settings

--- app/models/page.php
+++ app/models/page.php
@@ -1,27 +1,48 @@
 <?php
+
 class Page extends AppModel {
-	var $name = 'Page';
-	var $validate = array(
-		'slug' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'title' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+
+    public $name = 'Page';
+    public $validate = array(
+        'slug' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            //'message' => 'Your custom message here',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'title' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            //'message' => 'Your custom message here',
+            //'allowEmpty' => false,
+            //'required' => false,
+            //'last' => false, // Stop validation after this rule
+            //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
+    public $actsAs = array(
+        'Utils.Sluggable' => array(
+            'method' => 'multibyteSlug',
+            'separator' => '-',
+            'update' => true
+        )
+    );
+    public $displayField = 'title';
+
+    /**
+     * Constructor
+     *
+     * @param string $id ID
+     * @param string $table Table
+     * @param string $ds Datasource
+     */
+    public function __construct($id = false, $table = null, $ds = null) {
+        parent::__construct($id, $table, $ds);
+    }
+
 }
-?>
