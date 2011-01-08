b620ad3f391ce946fae043cd1cf42ac0c4f2a961
Andrea Dal Ponte <dalpo85@gmail.com>
2011-01-08 16:05:56 +0100

    news migration

--- app/config/migrations/map.php
+++ app/config/migrations/map.php
@@ -4,5 +4,6 @@ $map = array(
     1 => array('users' => 'M4d17de526cfc4c41ad210c5bdf9508da'),
     2 => array('menus' => 'Caa9bc72d1cc4476fbccp1e4beba7b4jk'),
     3 => array('pages' => 'M4d1qweasddsa5334sd210c5bdf9508da'),
-    4 => array('photo_galleries' => 'Maa9ro38dfjn49klsasdasd4beba7b4jk')
+    4 => array('photo_galleries' => 'Maa9ro38dfjn49klsasdasd4beba7b4jk'),
+    5 => array('news' => 'Ma09328k494kfdksdnvi8365hsd9KUdsk')
 );
---
+++ app/config/migrations/news.php
@@ -0,0 +1,50 @@
+<?php
+
+class Ma09328k494kfdksdnvi8365hsd9KUdsk extends CakeMigration {
+
+    public $dependendOf = array();
+    public $migration = array(
+        'up' => array(
+            'create_table' => array(
+                'news' => array(
+                    'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+                    'title' => array('type' => 'string', 'null' => false, 'default' => NULL),
+                    'slug' => array('type' => 'string', 'null' => false, 'default' => NULL),
+                    'short_description' => array('type' => 'string', 'null' => true, 'default' => NULL),
+                    'description' => array('type' => 'text', 'null' => true, 'default' => NULL),
+                    'published' => array('type' => 'boolean', 'null' => false, 'default' => false),
+                    'date' => array('type' => 'date', 'null' => false, 'default' => false),
+                    'date_from' => array('type' => 'date', 'null' => false, 'default' => false),
+                    'date_to' => array('type' => 'date', 'null' => false, 'default' => false),
+                    'language_id' => array('type' => 'int', 'null' => false, 'default' => false),
+                    'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+                    'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+                    'indexes' => array(
+                        'PRIMARY' => array('column' => 'id', 'unique' => 1),
+                        'TITLE' => array('column' => array('title'), 'unique' => 0),
+                        'SHORT_DESRCRIPTION' => array('column' => array('short_description'), 'unique' => 0),
+                        'SLUG' => array('column' => array('slug'), 'unique' => 1),
+                        'DATE' => array('column' => array('date'), 'unique' => 0),
+                        'DATE_FROM' => array('column' => array('date_from'), 'unique' => 0),
+                        'DATE_TO' => array('column' => array('date_to'), 'unique' => 0),
+                        'LANGUAGE' => array('column' => array('language_id'), 'unique' => 0),
+                        'PUBLISHED' => array('column' => array('published'), 'unique' => 0)
+                    )
+                )
+            )
+        ),
+        'down' => array(
+            'drop_table' => array(
+                'news'),
+        )
+    );
+
+    public function before($direction) {
+        return true;
+    }
+
+    public function after($direction) {
+        return true;
+    }
+
+}
