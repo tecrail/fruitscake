59df8ca913ddb8d24c736ba0a6a9ce2dc0ef83c1
Andrea Dal Ponte <dalpo85@gmail.com>
2011-01-08 13:45:09 +0100

    photo galleries index action

--- app/controllers/photo_galleries_controller.php
+++ app/controllers/photo_galleries_controller.php
@@ -1,61 +1,71 @@
 <?php
+
 class PhotoGalleriesController extends AppController {
 
-	public $name = 'PhotoGalleries';
+    public $name = 'PhotoGalleries';
 
-	public function admin_index() {
-		$this->PhotoGallery->recursive = 0;
-		$this->set('photoGalleries', $this->paginate());
-	}
+    public function admin_index() {
+        $this->paginate['PhotoGallery'] = array(
+            'contain' => array('Photo' => array(
+                'limit' => 1,
+                'conditions' => array('Photo.published' => true)
+            )),
+            'order' => array('PhotoGallery.modified' => 'DESC')
+        );
 
-	public function admin_view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid photo gallery', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('photoGallery', $this->PhotoGallery->read(null, $id));
-	}
+        $this->PhotoGallery->recursive = 0;
+        $this->set('photoGalleries', $this->paginate());
+    }
 
-	public function admin_add() {
-		if (!empty($this->data)) {
-			$this->PhotoGallery->create();
-			if ($this->PhotoGallery->save($this->data)) {
-				$this->Session->setFlash(__('The photo gallery has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The photo gallery could not be saved. Please, try again.', true));
-			}
-		}
-	}
+    public function admin_view($id = null) {
+        if (!$id) {
+            $this->Session->setFlash(__('Invalid photo gallery', true));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->set('photoGallery', $this->PhotoGallery->read(null, $id));
+    }
 
-	public function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid photo gallery', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->PhotoGallery->save($this->data)) {
-				$this->Session->setFlash(__('The photo gallery has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The photo gallery could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->PhotoGallery->read(null, $id);
-		}
-	}
+    public function admin_add() {
+        if (!empty($this->data)) {
+            $this->PhotoGallery->create();
+            if ($this->PhotoGallery->save($this->data)) {
+                $this->Session->setFlash(__('The photo gallery has been saved', true));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The photo gallery could not be saved. Please, try again.', true));
+            }
+        }
+    }
 
-	public function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for photo gallery', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->PhotoGallery->delete($id)) {
-			$this->Session->setFlash(__('Photo gallery deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Photo gallery was not deleted', true));
-		$this->redirect(array('action' => 'index'));
-	}
+    public function admin_edit($id = null) {
+        if (!$id && empty($this->data)) {
+            $this->Session->setFlash(__('Invalid photo gallery', true));
+            $this->redirect(array('action' => 'index'));
+        }
+        if (!empty($this->data)) {
+            if ($this->PhotoGallery->save($this->data)) {
+                $this->Session->setFlash(__('The photo gallery has been saved', true));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The photo gallery could not be saved. Please, try again.', true));
+            }
+        }
+        if (empty($this->data)) {
+            $this->data = $this->PhotoGallery->read(null, $id);
+        }
+    }
+
+    public function admin_delete($id = null) {
+        if (!$id) {
+            $this->Session->setFlash(__('Invalid id for photo gallery', true));
+            $this->redirect(array('action' => 'index'));
+        }
+        if ($this->PhotoGallery->delete($id)) {
+            $this->Session->setFlash(__('Photo gallery deleted', true));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Photo gallery was not deleted', true));
+        $this->redirect(array('action' => 'index'));
+    }
+
 }
--- app/views/photo_galleries/admin_index.ctp
+++ app/views/photo_galleries/admin_index.ctp
@@ -2,8 +2,7 @@
     <h2><?php __('Photo Galleries'); ?></h2>
     <table cellpadding="0" cellspacing="0">
         <tr>
-            <th><?php echo $this->Paginator->sort('id'); ?></th>
-            <th><?php echo $this->Paginator->sort('title'); ?></th>
+            <th colspan="2"><?php echo $this->Paginator->sort('title'); ?></th>
             <th><?php echo $this->Paginator->sort('published'); ?></th>
             <th><?php echo $this->Paginator->sort('modified'); ?></th>
             <th class="actions"><?php __('Actions'); ?></th>
@@ -17,26 +16,39 @@
             }
         ?>
             <tr<?php echo $class; ?>>
-                <td><?php echo $photoGallery['PhotoGallery']['id']; ?>&nbsp;</td>
+                <td>
+                    <div class="index-image-link">
+                    <?php
+                        echo $this->Html->link(" ",
+                                array('action' => 'view', $photoGallery['PhotoGallery']['id']),
+                                array(
+                                    'style' => "background-image: url(/img/photos/thumb.{$photoGallery['Photo'][0]['image']});",
+                                    'class' => 'image-link',
+                                    'title' => $photoGallery['PhotoGallery']['title']
+                                )
+                        )
+                    ?>
+                    </div>
+                </td>
                 <td><?php echo $photoGallery['PhotoGallery']['title']; ?>&nbsp;</td>
-                <td><?php echo $photoGallery['PhotoGallery']['published']; ?>&nbsp;</td>
+                <td style="text-align: center;"><?php echo $this->Backend->isActive($photoGallery['PhotoGallery']['published']); ?>&nbsp;</td>
                 <td><?php echo $photoGallery['PhotoGallery']['modified']; ?>&nbsp;</td>
                 <td class="actions">
-                <?php echo $this->Html->link(__('View', true), array('action' => 'view', $photoGallery['PhotoGallery']['id'])); ?>
-                <?php echo $this->Html->link(__('Edit', true), array('action' => 'edit', $photoGallery['PhotoGallery']['id'])); ?>
-                <?php echo $this->Html->link(__('Delete', true), array('action' => 'delete', $photoGallery['PhotoGallery']['id']), null, sprintf(__('Are you sure you want to delete # %s?', true), $photoGallery['PhotoGallery']['id'])); ?>
-            </td>
-        </tr>
+                    <?php echo $this->Html->link(__('View', true), array('action' => 'view', $photoGallery['PhotoGallery']['id'])); ?>
+                    <?php echo $this->Html->link(__('Edit', true), array('action' => 'edit', $photoGallery['PhotoGallery']['id'])); ?>
+                    <?php echo $this->Html->link(__('Delete', true), array('action' => 'delete', $photoGallery['PhotoGallery']['id']), null, sprintf(__('Are you sure you want to delete # %s?', true), $photoGallery['PhotoGallery']['id'])); ?>
+                </td>
+            </tr>
         <?php endforeach; ?>
-            </table>
+                </table>
 
     <?php echo $this->element("backend/paging") ?>
-            </div>            
-            <div class="actions">
-                <h3><?php __('Actions'); ?></h3>
-                <ul>
-                    <li><?php echo $this->Html->link(__('New Photo Gallery', true), array('action' => 'add')); ?></li>
-                    <li><?php echo $this->Html->link(__('List Photos', true), array('controller' => 'photos', 'action' => 'index')); ?> </li>
-                    <li><?php echo $this->Html->link(__('New Photo', true), array('controller' => 'photos', 'action' => 'add')); ?> </li>
+                </div>
+                <div class="actions">
+                    <h3><?php __('Actions'); ?></h3>
+                    <ul>
+                        <li><?php echo $this->Html->link(__('New Photo Gallery', true), array('action' => 'add')); ?></li>
+                        <li><?php echo $this->Html->link(__('List Photos', true), array('controller' => 'photos', 'action' => 'index')); ?> </li>
+                        <li><?php echo $this->Html->link(__('New Photo', true), array('controller' => 'photos', 'action' => 'add')); ?> </li>
     </ul>
 </div>
