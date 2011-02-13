574b98e9356c723f2266cf41609f1330132c80ea
Andrea Dal Ponte <dalpo85@gmail.com>
2011-02-13 16:36:54 +0100

    code cleanup

--- app/controllers/newsletter_users_controller.php
+++ app/controllers/newsletter_users_controller.php
@@ -1,111 +1,115 @@
 <?php
+
 class NewsletterUsersController extends AppController {
 
     public $name = 'NewsletterUsers';
     public $components = array('Email');
 
-    function beforeFilter() {
+    public function beforeFilter() {
         parent::beforeFilter();
 
-        /*$this->Email->smtpOptions = array(
-            'port'=>'25',
-            'timeout'=>'30',
-            'host' => 'out.alice.it'
-        );
-        $this->Email->delivery = 'smtp';*/
+        /* $this->Email->smtpOptions = array(
+          'port'=>'25',
+          'timeout'=>'30',
+          'host' => 'out.alice.it'
+          );
+          $this->Email->delivery = 'smtp'; */
 
         $this->Auth->allow(array('index', 'unsubscription', 'confirm_unsubscription'));
     }
 
-    function index() {
+    public function index() {
         if (!empty($this->data)) {
             $this->NewsletterUser->create();
             if ($this->NewsletterUser->save($this->data)) {
                 $this->Session->setFlash(__('lbl_thanks_for_your_subscription', true));
-                $this->redirect(array('action'=>'index'));
+                $this->redirect(array('action' => 'index'));
             } else {
                 $this->Session->setFlash(__('lbl_invalid_fields', true));
             }
         }
+        
         $this->set('options_languages', $this->NewsletterUser->Language->find('list'));
-        $this->pageTitle = $this->pageTitle." :: ".__('lbl_newsletter_subscription', true);
-//        $this->pageTitle = ((__('lbl_newsletter', true)).' - '.(__('lbl_newsletter_subscription', true)).' - '.(__('lbl_title', true)));
     }
 
-    function unsubscription() {
+    public function unsubscription() {
         if (!empty($this->data)) {
-            $this->NewsletterUser->set( $this->data );
-//            debug($this->data);
-//            die();
-            //annulla le validazioni dell'inserimento
-            unset($this->NewsletterUser->validate['email']['email_unique']);
+            $this->NewsletterUser->set($this->data);
+
+            // @todo: check this validations
+            unset($this->NewsletterUser->validate['email']['email_unique']);             
             unset($this->NewsletterUser->validate['first_name']);
             unset($this->NewsletterUser->validate['last_name']);
 
-            if($this->NewsletterUser->validates()) {
+            if ($this->NewsletterUser->validates()) {
+
                 $newsletterUser = $this->NewsletterUser->find('first', array('conditions' => array('NewsletterUser.email' => $this->data['NewsletterUser']['email'])));
+
                 $this->set('newsletterUser', $newsletterUser);
-                if($newsletterUser) {
-                    //invia un email di conferma
+                if ($newsletterUser) {
+                    
                     if (!$this->_sendUnsubscriptionMail($newsletterUser)) {
-                            $this->Session->setFlash(__('lbl_email_not_sent', true));
-                            return;
+                        $this->Session->setFlash(__('lbl_unsubscription_email_not_sent', true));
+                        return;
                     }
-                    $this->Session->setFlash(__('lbl_email_to_remove', true));
+                    $this->Session->setFlash(__('lbl_unsubscription_email_to_remove', true));
+                    
                 } else {
-                    $this->Session->setFlash(__('lbl_email_not_found', true));
+                    $this->Session->setFlash(__('lbl_unsubscription_email_not_found', true));
                 }
+
             } else {
-                $this->Session->setFlash(__('lbl_email_not_found', true));
+                $this->Session->setFlash(__('lbl_unsubscription_email_not_found', true));
             }
+            
         }
-        $this->pageTitle = $this->pageTitle." :: ".__('lbl_delete_from_newsletter', true);
     }
 
-    function confirm_unsubscription($id = null) {
-        if($id) {
+    public function confirm_unsubscription($id = null) {
+        if ($id) {
             $newsletterUser = $this->NewsletterUser->read(null, $id);
-            if($newsletterUser) {
-                $key = md5($newsletterUser['NewsletterUser']['id'].$newsletterUser['NewsletterUser']['email']);
+            if ($newsletterUser) {
+                $key = md5($newsletterUser['NewsletterUser']['id'] . $newsletterUser['NewsletterUser']['email']);
                 $paramKey = '';
-                if(isset($this->params['named']['key'])) { $paramKey = $this->params['named']['key']; }
-                if( $key == $paramKey ) {
+                if (isset($this->params['named']['key'])) {
+                    $paramKey = $this->params['named']['key'];
+                }
+                if ($key == $paramKey) {
                     $this->NewsletterUser->del($id);
                     $this->Session->setFlash(__('lbl_unsubscription_ok', true));
                     $this->redirect('unsubscription');
                 } else {
-                    $this->redirect(array('action'=>'index'));
+                    $this->redirect(array('action' => 'index'));
                 }
-            }else {
-                $this->redirect(array('action'=>'index'));
+            } else {
+                $this->redirect(array('action' => 'index'));
             }
         } else {
-            $this->redirect(array('action'=>'index'));
+            $this->redirect(array('action' => 'index'));
         }
-
     }
 
-    function admin_index() {
+    public function admin_index() {
         $this->NewsletterUser->recursive = 0;
         $this->set('newsletterUsers', $this->paginate());
     }
 
-    function admin_view($id = null) {
+    public function admin_view($id = null) {
         if (!$id) {
-            $this->Session->setFlash(__('Iscritto alla newsletter non trovato', true));
-            $this->redirect(array('action'=>'index'));
+            $this->Session->setFlash(__('lbl_newsletter_user_not_found', true));
+            $this->redirect(array('action' => 'index'));
         }
         $this->set('newsletterUser', $this->NewsletterUser->read(null, $id));
     }
 
-    function admin_add() {
+    public function admin_add() {
         if (!empty($this->data)) {
             $this->NewsletterUser->create();
             if ($this->NewsletterUser->save($this->data)) {
-                $this->Session->setFlash(__('Iscritto alla newsletter salvato', true));
-                $this->redirect(array('action'=>'index'));
+                $this->Session->setFlash(__('lbl_newsletter_user_saved', true));
+                $this->redirect(array('action' => 'index'));
             } else {
-                $this->Session->setFlash(__('Sono presenti uno o più campi non validi.', true));
+                $this->Session->setFlash(__('lbl_invalid_fields', true));
             }
         } else {
             $this->data['NewsletterUser']['is_active'] = true;
@@ -113,17 +117,17 @@ class NewsletterUsersController extends AppController {
         $this->set('languages', $this->NewsletterUser->Language->find('list'));
     }
 
-    function admin_edit($id = null) {
+    public function admin_edit($id = null) {
         if (!$id && empty($this->data)) {
-            $this->Session->setFlash(__('Iscritto alla newsletter non trovato', true));
-            $this->redirect(array('action'=>'index'));
+            $this->Session->setFlash(__('lbl_newsletter_user_not_found', true));
+            $this->redirect(array('action' => 'index'));
         }
         if (!empty($this->data)) {
             if ($this->NewsletterUser->save($this->data)) {
-                $this->Session->setFlash(__('Iscritto alla newsletter salvato', true));
-                $this->redirect(array('action'=>'index'));
+                $this->Session->setFlash(__('lbl_newsletter_user_saved', true));
+                $this->redirect(array('action' => 'index'));
             } else {
-                $this->Session->setFlash(__('Sono presenti uno o più campi non validi.', true));
+                $this->Session->setFlash(__('lbl_invalid_fields', true));
             }
         }
         if (empty($this->data)) {
@@ -132,45 +136,41 @@ class NewsletterUsersController extends AppController {
         $this->set('languages', $this->NewsletterUser->Language->find('list'));
     }
 
-    function admin_delete($id = null) {
+    public function admin_delete($id = null) {
         if (!$id) {
-            $this->Session->setFlash(__('Iscritto alla newsletter non trovato', true));
-            $this->redirect(array('action'=>'index'));
+            $this->Session->setFlash(__('lbl_newsletter_user_not_found', true));
+            $this->redirect(array('action' => 'index'));
         }
         if ($this->NewsletterUser->del($id)) {
-            $this->Session->setFlash(__('Iscritto alla newsletter cancellato', true));
-            $this->redirect(array('action'=>'index'));
+            $this->Session->setFlash(__('lbl_newsletter_user_deleted', true));
+            $this->redirect(array('action' => 'index'));
         }
     }
 
     protected function _sendUnsubscriptionMail($newsletterUser = null) {
-        if($newsletterUser) {
+        if ($newsletterUser) {
+            
             $this->Email->to = $newsletterUser['NewsletterUser']['email'];
-            $this->Email->subject = __('lbl_request_to_remove :: www.castellosuperiore.it'); //TODO: lbl
-            $this->Email->replyTo = REPLYTO_EMAIL;
-            $this->Email->from = BASE_TITLE.'<'.INFO_EMAIL.'>';
-            if ($newsletterUser['NewsletterUser']['language_id'] == 1) {
-                $this->Email->layout = 'unsubscription';
-                $this->Email->template = 'unsubscription';
-            }
-            else {
-                $this->Email->layout = 'unsubscription_en';
-                $this->Email->template = 'unsubscription_en';
-            }
-            //Send as 'html', 'text' or 'both' (default is 'text')
-            $this->Email->sendAs = 'both'; // because we like to send pretty mail
+
+            $this->Email->subject = Configure::read('App.baseTitle') . " :: " . __('lbl_newsletter_unsubscription_mail_subject', true);
+            $this->Email->replyTo = Configure::read('App.baseTitle') . ' <' . Configure::read('App.defaultEmail') . '>';
+            $this->Email->from = Configure::read('App.baseTitle') . ' <' . Configure::read('App.defaultEmail') . '>';
+
+            $this->Email->layout = 'unsubscription';
+            $this->Email->template = 'unsubscription';
+
+            $this->Email->sendAs = 'both';  //Send as 'html', 'text' or 'both' (default is 'text')
+
             //Set view variables as normal
-            $key = md5($newsletterUser['NewsletterUser']['id'].$newsletterUser['NewsletterUser']['email']);
-            $this->set('key', $key);
+            $token = md5($newsletterUser['NewsletterUser']['id'] . $newsletterUser['NewsletterUser']['email']);
+            
+            $this->set('key', $token);
             $this->set('newsletterUser', $newsletterUser);
-             
-            //Do not pass any args to send()
+
             return $this->Email->send();
         } else {
             return false;
         }
-
     }
 
 }
-?>
--- app/controllers/newsletters_controller.php
+++ app/controllers/newsletters_controller.php
@@ -1,4 +1,5 @@
 <?php
+
 class NewslettersController extends AppController {
 
     public $name = 'Newsletters';
@@ -8,12 +9,12 @@ class NewslettersController extends AppController {
     public function beforeFilter() {
         parent::beforeFilter();
         //debug
-        /*$this->Email->smtpOptions = array(
-            'port'=>'25',
-            'timeout'=>'30',
-            'host' => 'out.alice.it'
-        );
-        $this->Email->delivery = 'smtp';*/
+        /* $this->Email->smtpOptions = array(
+          'port'=>'25',
+          'timeout'=>'30',
+          'host' => 'out.alice.it'
+          );
+          $this->Email->delivery = 'smtp'; */
         $this->Auth->allow(array('view'));
     }
 
@@ -22,39 +23,26 @@ class NewslettersController extends AppController {
         $this->set('newsletters', $this->paginate());
     }
 
-    #public function admin_view($id = null) {
-    #    if (!$id) {
-    #        $this->Session->setFlash(__('Newsletter non trovata', true));
-    #        $this->redirect(array('action'=>'index'));
-    #    }
-    #    $this->set('newsletter', $this->Newsletter->read(null, $id));
-    #}
-
     public function admin_add() {
         if (!empty($this->data)) {
             $this->Newsletter->create();
-            
 
             $this->data['Newsletter']['user_id'] = $this->Session->read('Auth.User.id');
+
             if ($this->Newsletter->save($this->data)) {
-                
-                                    //da verificare
-            if ($this->data['Newsletter']['language_id'] == 1)
-                    $this->data['Newsletter']['description_text'] = "Per visualizzare correttamente la newsletter si prega di utillizzare il seguente link: http://".$_SERVER['HTTP_HOST'].'/newsletters/view/'.$this->Newsletter->id;
-            else
-                $this->data['Newsletter']['description_text'] = "To properly view the newsletter please use the following link: http://".$_SERVER['HTTP_HOST'].'/newsletters/view/'.$this->Newsletter->id;
-            
+
+                $this->data['Newsletter']['description_text'] = __("To properly view the newsletter please use the following link") . ": " . Router::url(array('controller' => 'newsletters', 'action' => 'view', $this->Newsletter->id), true);
                 $this->Newsletter->save($this->data);
-                
-                $this->Session->setFlash(__('La Newsletter è stata salvata', true));
-                if($this->data['Newsletter']['send_newsletter']) {
-                //                        $this->Session->setFlash($this->_sendNewsletter($this->data));
-                    $this->redirect(array('action'=>'deliver', 'id' => $this->Newsletter->id ));
+
+                $this->Session->setFlash(__('lbl_newsletter_saved', true));
+
+                if ($this->data['Newsletter']['send_newsletter']) {
+                    $this->redirect(array('action' => 'deliver', 'id' => $this->Newsletter->id));
                 } else {
-                    $this->redirect(array('action'=>'index'));
+                    $this->redirect(array('action' => 'index'));
                 }
             } else {
-                $this->Session->setFlash(__('Sono presenti uno o più campi non validi.', true));
+                $this->Session->setFlash(__('lbl_invalid_fields', true));
             }
         } else {
             $this->data['Newsletter']['user_id'] = $this->Session->read('Auth.User.id');
@@ -70,40 +58,44 @@ class NewslettersController extends AppController {
 
     public function admin_edit($id = null) {
         if (!$id && empty($this->data)) {
-            $this->Session->setFlash(__('Newsletter non trovata', true));
-            $this->redirect(array('action'=>'index'));
+            $this->Session->setFlash(__('lbl_newsletter_not_found', true));
+            $this->redirect(array('action' => 'index'));
         }
+
         if (!empty($this->data)) {
-            if ($this->data['Newsletter']['language_id'] == 1) //italiano
-                    $this->data['Newsletter']['description_text'] = "Per visualizzare correttamente la newsletter si prega di utillizzare il seguente link: http://".$_SERVER['HTTP_HOST'].'/newsletters/view/'.$this->data['Newsletter']['id'];
-            else
-                $this->data['Newsletter']['description_text'] = "To properly view the newsletter please use the following link: http://".$_SERVER['HTTP_HOST'].'/newsletters/view/'.$this->data['Newsletter']['id'];
+
+            $this->data['Newsletter']['description_text'] = __("To properly view the newsletter please use the following link") . ": " . Router::url(array('controller' => 'newsletters', 'action' => 'view', $this->Newsletter->id), true);
+
             if ($this->Newsletter->save($this->data)) {
-                $this->Session->setFlash(__('La Newsletter è stata salvata', true));
-                if($this->data['Newsletter']['send_newsletter']) {
-                //                        $this->Session->setFlash($this->_sendNewsletter($this->data));
-                    $this->redirect(array('action'=>'deliver', 'id' => $this->Newsletter->id ));
+
+                $this->Session->setFlash(__('lbl_newsletter_saved', true));
+
+                if ($this->data['Newsletter']['send_newsletter']) {
+                    $this->redirect(array('action' => 'deliver', 'id' => $this->Newsletter->id));
                 } else {
-                    $this->redirect(array('action'=>'index'));
+                    $this->redirect(array('action' => 'index'));
                 }
             } else {
-                $this->Session->setFlash(__('Sono presenti uno o più campi non validi.', true));
+                $this->Session->setFlash(__('lbl_invalid_fields', true));
             }
         }
+
         if (empty($this->data)) {
             $this->data = $this->Newsletter->read(null, $id);
             $this->data['Newsletter']['user_id'] = $this->Session->read('Auth.User.id');
         }
+
         $users = $this->Newsletter->User->find('list');
         $languages = $this->Newsletter->Language->find('list');
+
         $this->set(compact('users'));
         $this->set(compact('languages'));
     }
 
     public function admin_deliver($id = null) {
         if (!$id) {
-            $this->Session->setFlash(__('Newsletter non trovata', true));
-            $this->redirect(array('action'=>'index'));
+            $this->Session->setFlash(__('lbl_newsletter_not_found', true));
+            $this->redirect(array('action' => 'index'));
         }
 
         $newsletter = $this->Newsletter->read(null, $id);
@@ -116,59 +108,27 @@ class NewslettersController extends AppController {
             foreach ($newsletterUsers as $newsletterUser) {
                 $this->_deliverEmail($newsletter, $newsletterUser['NewsletterUser']);
             }
-            
-//            debug($this->Email->smtpError);
-//            die();
 
-            $this->Session->setFlash(__('Newsletter inviata', true));
+            $this->Session->setFlash(__('lbl_newsletter_sent', true));
 
-            $this->redirect(array('action'=>'index'));
-
-
+            $this->redirect(array('action' => 'index'));
         }
-
     }
 
-    //    protected public function _sendNewsletter($data = null, $languages) {
-    //        $msg = "Utenti a cui non è stato possibile recapitare la newsletter:<ul>";
-    //        $sentToAll = true;
-    //
-    //        $newsletterUsers = $this->NewsletterUser->find('all', array('conditions' => array('NewsletterUser.is_active' => true)));
-    //        foreach ($newsletterUsers as $newsletterUser) {
-    //            $this->Email->reset();
-    //            if(!$this->_deliverEmail($data, $newsletterUser['NewsletterUser']['email'])) {
-    //                $sentToAll = false;
-    //                $msg.= "<li>{$newsletterUser['NewsletterUser']['email']}</li>";
-    //            }
-    //        }
-    //        if($sentToAll) {
-    //            $msg = "Newsletter inviata con successo";
-    //        } else {
-    //            $msg.= "</ul>";
-    //        }
-    //
-    //        return $msg;
-    //
-    //    }
-
     protected function _deliverEmail($newsletter = null, $user = null, $lang = null) {
-        if($newsletter && $user) {
+        if ($newsletter && $user) {
 
             $this->Email->to = $user['email'];
-            $this->Email->subject = 'Newsletter :: '.$newsletter['Newsletter']["title"];
-            $this->Email->replyTo = REPLYTO_EMAIL;
-            $this->Email->from = BASE_TITLE.'<'.INFO_EMAIL.'>';
 
-            if((bool)$newsletter['Newsletter']["attachment"]) {
+            $this->Email->subject = Configure::read('App.baseTitle') . " :: " . $newsletter['Newsletter']["title"];
+            $this->Email->replyTo = Configure::read('App.baseTitle') . ' <' . Configure::read('App.defaultEmail') . '>';
+            $this->Email->from = Configure::read('App.baseTitle') . ' <' . Configure::read('App.defaultEmail') . '>';
+
+            if ((bool) $newsletter['Newsletter']["attachment"]) {
                 $this->Email->attachments = array(APP . "webroot" . DS . "files" . DS . "attachments" . DS . $newsletter['Newsletter']["attachment"]);
             }
 
-            if ($newsletter['Newsletter']["language_id"] == 1) {
-                $this->Email->layout = 'newsletter';
-            }
-            else {
-                $this->Email->layout = 'newsletter_en';
-            }
+            $this->Email->layout = 'newsletter';
             $this->Email->template = 'newsletter';
 
             $this->Email->sendAs = 'both';
@@ -176,12 +136,7 @@ class NewslettersController extends AppController {
             $this->set("data", $newsletter);
             $this->set("user", $user);
 
-//            debug
-//            $this->Email->delivery = 'smtp';
-
-
             return $this->Email->send();
-
         } else {
             return false;
         }
@@ -189,24 +144,26 @@ class NewslettersController extends AppController {
 
     public function view($id = null) {
         if (!$id) {
-            $this->Session->setFlash(__('Newsletter non trovata', true));
-            $this->redirect(array('action'=>'index'));
+            $this->Session->setFlash(__('lbl_newsletter_not_found', true));
+            $this->redirect(array('action' => 'index'));
         }
+        
         $this->layout = 'email/html/newsletter';
         $this->set('newsletter', $this->Newsletter->read(null, $id));
     }
 
-
     public function admin_delete($id = null) {
         if (!$id) {
-            $this->Session->setFlash(__('Invalid id for Newsletter', true));
+            $this->Session->setFlash(__('lbl_newsletter_not_found', true));
             $this->redirect(array('action' => 'index'));
         }
+
         if ($this->Newsletter->del($id)) {
-            $this->Session->setFlash(__('Newsletter deleted', true));
+            $this->Session->setFlash(__('lbl_newsletter_deleted', true));
             $this->redirect(array('action' => 'index'));
         }
-        $this->Session->setFlash(__('The Newsletter could not be deleted. Please, try again.', true));
+        
+        $this->Session->setFlash(__('lbl_newsletter_could_not_be_deleted', true));
         $this->redirect(array('action' => 'index'));
     }
 
